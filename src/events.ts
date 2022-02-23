e7082676510c727c4e62163f155b1adf355f0033
Ciensprog <jorg.leonrd@gmail.com>
2022-02-22 18:15:29 -0600

    Export common events

---
+++ src/events.ts
@@ -0,0 +1,14 @@
+import { MouseEvent } from 'react'
+
+/*
+|--------------------------------------------------------------------------
+| Events
+|--------------------------------------------------------------------------
+|
+| ...
+|
+*/
+
+export const onClickOnLink = (event: MouseEvent<HTMLAnchorElement>) => {
+  event.preventDefault()
+}
