55b3bc61084850f857fea0dd7c706f3db8102ed0
Ciensprog <jorg.leonrd@gmail.com>
2022-02-22 18:18:47 -0600

    Import new styles file

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 
 import 'virtual:svg-icons-register'
-import './index.css'
+import './index.scss'
 
 /*
 |--------------------------------------------------------------------------
